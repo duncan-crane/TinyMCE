ababf6d1cff5c003d66d2a7beae8d2c0973f0b36
aoxomoxoa <duncan.crane@aoxomoxoa.co.uk>
2021-02-19 09:47:16 +0000

    Release 1.0.6 19/02/2021
    Contains a fix for:
    T7032 Editor not loading in page view mode

--- custom_plugins/mediawiki/plugins/mw_wikiparser/plugin.js
+++ custom_plugins/mediawiki/plugins/mw_wikiparser/plugin.js
@@ -4364,6 +4364,89 @@ _pipeText;
 	 * @param {tinymce.LoadContentEvent} e
 	 */
 	function _onLoadContent(e) {
+//		tinymce.activeEditor.on('ScriptsLoaded', function(e) {
+			var _toolbarResizeFactor = tinymce.activeEditor.getParam("toolbarResize");
+	  
+			/**
+			 * dynamicallyAccessCSS 
+			 *
+			 * @link    https://github.com/Frazer/dynamicallyAccessCSS.js
+			 * @license MIT
+			 *          
+			 * @author  Frazer Kirkman
+			 * @published 2016
+			 */
+			  
+			var returnStyleSheetRules = (function (){  
+				if(!document.styleSheets[0]){
+					// Create the <style> tag
+					var style = document.createElement("style");
+					// WebKit hack :(
+					style.appendChild(document.createTextNode(""));
+					// Add the <style> element to the page
+					document.head.appendChild(style);
+				  
+				}
+				if(document.styleSheets[0].cssRules){
+					return function (item) {return  item.cssRules;}
+				} else if (document.styleSheets[0].rules) {
+					return function (item) {return  item.rules;}
+				}
+			})();
+			  
+			function getCSSRule(search, returnArray) {  
+				let styleSheets = [].map.call(document.styleSheets, function(item) {
+					return [].slice.call(returnStyleSheetRules(item));
+				});
+	  
+				let rule = null;
+				let rules = [];
+				styleSheets.forEach(function(thisSheet){
+					let findTheRule = thisSheet.filter(function(rule) {
+						if(rule.selectorText){
+							if(rule.selectorText == search){
+								return rule.selectorText.indexOf(search)>=0;	
+							}else return false;
+						}
+					});
+				  
+					if(findTheRule.length){
+						  rules = rules.concat(findTheRule);
+						  rule = findTheRule[findTheRule.length-1];    //findTheRule will contain all rules that reference the selector. findTheRule[findTheRule.length-1] contains the last rule.
+					}
+				});
+
+				if (rule){
+					if(returnArray){
+						return rules;
+					}else{
+						return rule;
+					}
+				}else{
+					let sheet = document.styleSheets[0];   //if the rule we are looking for doesn't exist, we create it
+					var pos = sheet.cssRules.length;
+					rule = search + "{  }";
+					sheet.insertRule( rule ,pos );
+					rule = sheet.cssRules[pos];
+				}
+	
+				if(returnArray){
+					returnStyleSheetRules(document.styleSheets[0]);
+					return rules = rules.concat(rule);
+				}else{
+					returnStyleSheetRules(document.styleSheets[0])[pos];
+					return rule;
+				}
+			}
+
+			var myRules  = getCSSRule('.tox-tbtn', true);
+			myRules.forEach(function(thisRule){
+				thisRule.style.transform = "scale(" + _toolbarResizeFactor + ")";
+				thisRule.style.setProperty ("height", 34 * _toolbarResizeFactor + "px", "important");
+				thisRule.style.setProperty ("width", "auto", "important");
+			});
+//		});
+
 		return;
 	}
 
